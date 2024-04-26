02fd69d13d7a71a796b738c75aaa3ce0644a7788
ShwetaMonocept <shweta@monocept.com>
2024-04-26 12:05:57 +0530

    Initial Commit

---
+++ PaginationBatchingChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaginationBatchingChallenge
+//
+//  Created by Shweta Lodhe on 26/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
